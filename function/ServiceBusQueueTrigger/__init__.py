561b6e26b4c58fb9208c4b9f6f2ffbdd440042ad
tung nguyen <tung.nguyenthanh@petronas.com>
2022-08-03 17:21:48 +0700

    update

---
+++ function/ServiceBusQueueTrigger/__init__.py
@@ -0,0 +1,57 @@
+import logging
+from multiprocessing import connection
+
+from colorama import Cursor
+import azure.functions as func
+import psycopg2
+import os
+from datetime import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def main(msg: func.ServiceBusMessage):
+
+    notification_id = int(msg.get_body().decode('utf-8'))
+    logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
+
+    # TODO: Get connection to database
+    connection = psycopg2.connect(
+        host="uadproject3dbserver.postgres.database.azure.com",
+        dbname="techconfdb",
+        user="udaadmin",
+        password="Matkhaulan#1")
+    cur = connection.cursor()    
+
+    try:
+        # TODO: Get notification message and subject from database using the notification_id
+        message_subject =  cur.execute
+        (
+            "select message, subject from notification" 
+            +
+            "where id = {}".format(notification_id)
+        )
+
+        # TODO: Get attendees email and name
+        cur.execute("select first_name, last_name, email from attendee")
+
+        # TODO: Loop through each attendee and send an email with a personalized subject
+        attendees = cur.fetchall()
+        for attendee in attendees: 
+            Mail('{},{},{}'.format({'admin@techconf.com'},{attendee[2]},{message_subject}))
+        
+        # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
+        notification_sent_date = datetime.utcnow()
+        notification_status = 'Total number of attendees notified: {}'.format(len(attendees))
+        cur.execute("update notification" 
+                    + 
+                    "set status = '{}', completed_date = '{}' where id = {};".format(notification_status, notification_sent_date, notification_id))
+        
+        connection.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        connection.rollback()
+    finally:
+        # TODO: Close connection
+        cur.close()
+        connection.close()
--- web/config.py
+++ web/config.py
@@ -4,18 +4,18 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL="uadproject3dbserver.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="udaadmin" #TODO: Update value
+    POSTGRES_PW="Matkhaulan#1"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
-    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
-    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://udaproject3bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5uSCo6H67JsTk+k6mCA5zT6RIu+rqwifngevsjjF2iM=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notification'
+    ADMIN_EMAIL_ADDRESS: 'miracle15794@gmail.com'
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
